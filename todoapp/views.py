4d4edc4d352ba2f35c328e8debaed339f4ac42e8
Glazochek <an9680411@gmail.com>
2022-08-27 11:52:51 +0300

    dz-3.4

---
+++ todoapp/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, get_object_or_404
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .models import TODO, Project
+from .serializers import ProjectSerializers, TODOSerializers
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.decorators import action
+
+
+def main(request):
+    context = {}
+    return render(request, 'todoapp/index.html', context)
+
+
+class ALimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class BLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
+
+class ProjectViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializers
+    pagination_class = ALimitOffsetPagination
+
+    @action(detail=True, methods=['get'])
+    def article_name_only(self, request, pk=None):
+        article = get_object_or_404(Project, pk=pk)
+        return Response({'-': article.name})
+
+    def list(self, request):
+        articles = Project.objects.all()
+        serializer = ProjectSerializers(articles, many=True)
+        return Response(serializer.data)
+
+
+class TODOViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    queryset = TODO.objects.all()
+    serializer_class = TODOSerializers
+    pagination_class = BLimitOffsetPagination
+
+    @action(detail=True, methods=['get'])
+    def article_name_only(self, request, pk=None):
+        article = get_object_or_404(TODO, pk=pk)
+        return Response({'-': article.text})
+
+
+
